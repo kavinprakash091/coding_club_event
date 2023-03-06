ef107c485322d4a7391e1e05d72ba3aae974300c
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-03-06 21:57:28 +0530

    userModel name correct

--- back_end/routes/UserRouter.js
+++ back_end/routes/UserRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import User from '../models/userModel.js';
+import User from '../models/UserModel.js';
 import expressAsyncHandler from 'express-async-handler';
 import { generateToken } from '../utils.js';
 
